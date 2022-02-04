c364e397a8e927596b733d7c3ba11e248f8754fc
DawidKopecki <78148257+DawidKopecki@users.noreply.github.com>
2022-02-04 22:44:09 +0000

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'To Do';
+  thingsToDo = [
+    'Learn JavaScript',
+    'Learn Angular',
+    'Learn Redux'
+  ];
+  thingsCompleted = []
+    summary(): string {
+      return `${this.thingsToDo.length} to do / ${this.thingsCompleted.length} done`;
+    }
+  
+  constructor() {
+    setInterval(() => {
+    this.thingsToDo.push('make coffee');
+    },1000);
+  }
+}
